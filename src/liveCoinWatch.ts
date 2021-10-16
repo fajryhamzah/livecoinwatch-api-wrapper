9c14dee7704ec56d3055a74288782f5c8ab397fd
fajry hamzah <fhaji.jakmall@gmail.com>
2021-10-16 21:18:35 +0700

    implement all endpoint API client

--- src/liveCoinWatch.ts
+++ src/liveCoinWatch.ts
@@ -1,56 +1,169 @@
 'use strict';
 
 import axios from 'axios';
-import CreditResponse from './responses/creditResponse';
 
 const API_URL = 'https://api.livecoinwatch.com/';
 
 export default class LiveCoinWatch {
     constructor(apiKey: string) {
-        axios.defaults.headers.post['content-type'] = 'application/json';
         axios.defaults.headers.post['x-api-key'] = apiKey;
     }
-    
+
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#credits
+     * @returns {Promise}
+     */
     public getCredits() {
-        return this.getResponse('credits').then(data => {
-            return new CreditResponse(data);
-        });
- 
+        return this.getResponse('credits')
     }
 
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#fiatsall
+     * @returns {Promise}
+     */
     public getAllFiats() {
-
+        return this.getResponse('fiats/all');
     }
 
-    public getOverview(currency: string) {
-
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#overview
+     * @param {string} fiat 
+     * @returns {Promise}
+     */
+    public getOverview(fiat: string) {
+        return this.getResponse('overview', {
+            currency: fiat
+        });
     }
 
-    public getOverviewHistory(currency: string, startTimestamp: number, endTimestamp: number) {
-
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#overviewhistory
+     * @param {string} fiat 
+     * @param {number} startTimestamp 
+     * @param {number} endTimestamp 
+     * @returns {Promise}
+     */
+    public getOverviewHistory(fiat: string, startTimestamp: number, endTimestamp: number) {
+        return this.getResponse('overview/history', {
+            currency: fiat,
+            end: endTimestamp,
+            start: startTimestamp
+        });
     }
 
-    public getCoin(currency: string, coinCode: string, withMetadata: boolean = false) {
-
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#coinssingle
+     * @param {string} fiat 
+     * @param {string} coinCode 
+     * @param {boolean} withMetadata 
+     * @returns {Promise}
+     */
+    public getCoin(fiat: string, coinCode: string, withMetadata: boolean = false) {
+        return this.getResponse('coins/single', {
+            currency: fiat,
+            code: coinCode.toUpperCase(),
+            meta: withMetadata
+        });
     }
 
-    public getCoinHistory(currency: string, coinCode: string, startTimestamp: number, endTimestamp: number) {
-
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#coinssinglehistory
+     * @param {string} fiat 
+     * @param {string} coinCode 
+     * @param {number} startTimestamp 
+     * @param {number} endTimestamp 
+     * @returns {Promise}
+     */
+    public getCoinHistory(fiat: string, coinCode: string, startTimestamp: number, endTimestamp: number) {
+        return this.getResponse('coins/single/history', {
+            currency: fiat,
+            code: coinCode.toUpperCase(),
+            start: startTimestamp,
+            end: endTimestamp
+        });
     }
 
-    public getCoinList(currency: string, sort: string = 'rank', order: 'ascending', offset: number = 0, limit: number = 50, withMeta: boolean = true) {
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#coinslist
+     * @param {string} fiat 
+     * @param {object|null} config 
+     * @returns {Promise}
+     */
+    public getCoinList(
+        fiat: string,
+        config: {
+            sort?: string;
+            order?: string;
+            offset?: number;
+            limit?: number;
+            meta?: boolean;
+        } | null = null
+    ) {
+        let defaultConfig = {
+            sort: 'rank',
+            order: 'ascending',
+            offset: 0,
+            limit: 50,
+            meta: true
+        };
 
+        let mergeConfig = { ...defaultConfig, ...config, ...{ currency: fiat } };
+
+        return this.getResponse('coins/list', mergeConfig);
     }
 
-    public getExchange(currency: string, exchangeCode = 'binance', withMeta: boolean = true) {
-
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#exchangessingle
+     * @param {string} fiat 
+     * @param {string} exchangeCode 
+     * @param {boolean} withMeta 
+     * @returns {Promise} 
+     */
+    public getExchange(fiat: string, exchangeCode = 'binance', withMeta: boolean = true) {
+        return this.getResponse('exchanges/single', {
+            currency: fiat,
+            code: exchangeCode,
+            meta: withMeta
+        });
     }
 
-    public getExchangeList(currency: string = 'USD', sort: string = 'volume', order: string = 'descending', offset: number = 0, limit: number = 50, withMeta: boolean = true) {
+    /**
+     * Wrapper for endpoint https://livecoinwatch.github.io/lcw-api-docs/#exchangeslist
+     * @param {string} fiat 
+     * @param {object|null} config 
+     * @returns {Promise}
+     */
+    public getExchangeList(
+        fiat: string = 'USD',
+        config: {
+            sort?: string;
+            order?: string;
+            offset?: number;
+            limit?: number;
+            meta?: boolean;
+        } | null = null
+    ) {
+        let defaultConfig = {
+            sort: 'volume',
+            order: 'descending',
+            offset: 0,
+            limit: 50,
+            meta: true
+        };
 
+        let mergeConfig = { ...defaultConfig, ...config, ...{ currency: fiat } };
+
+
+        return this.getResponse('exchanges/list', mergeConfig);
     }
 
-    private getResponse(endpoint: string, data: any = null) {
+    /**
+     * Get response from the API
+     * @param {string} endpoint 
+     * @param {any} data 
+     * @returns {Promise}
+     */
+    private getResponse(endpoint: string, data: any = {}) {
         return axios.post(API_URL + endpoint, data).then(response => {
             return response.data;
         });
